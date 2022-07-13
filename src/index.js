56fe8de5f7b4c584fce822c04179d8dc29f506ff
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-07-13 21:42:52 +0300

    add library and html

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './css/styles.css';
+import { FetchImagesService } from './js/fetchImagesService';
+import { refs } from './js/getRefs';
+import { LoadMoreBtn } from './js/load-more-btn';
+import { makeImageMarkup } from './js/markupService';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
