0e247b9e2238cf21a63ab3896bb63e82b35a8f21
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-07-14 11:25:44 +0300

    class FetchImagesService

--- src/js/fetchImagesService.js
+++ src/js/fetchImagesService.js
@@ -2,3 +2,26 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://pixabay.com/api';
 const API_KEY = '28612573-96fa08825695f2b47097a163d';
 const PARAM = 'per_page=40&orientation=horizontal&image_type=photo&safesearch=true';
+
+class FetchImagesService {
+    constructor(){
+        this.searchQuery = '';
+        this.page = 1;
+    }
+    async fetchImages () {
+        try{
+            const response = await axios.get(`/?key=${API_KEY}&q=${this.searchQuery}&page=${this.page}&${PARAM}`);
+            this.incrementPage();
+            return response;        
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    incrementPage() {
+        this.page += 1;
+    }
+    resetPage() {
+        this.page = 1;
+    }
+}
+export { FetchImagesService };
