541af3d9edbe182901d517ed7c99bd4d428ad523
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-07-14 15:52:03 +0300

    add js file structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './css/styles.css';
 import { FetchImagesService } from './js/fetchImagesService';
-// import { refs } from './js/getRefs';
-// import { LoadMoreBtn } from './js/load-more-btn';
-// import { makeImageMarkup } from './js/markupService';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
+import { LoadMoreBtn } from './js/loadMoreBtn';
+import { makeImageMarkup } from './js/markupService';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { getRefs } from './js/getRefs';
+const refs = getRefs();
--- src/js/fetchImagesService.js
+++ src/js/fetchImagesService.js
@@ -3,7 +3,7 @@ axios.defaults.baseURL = 'https://pixabay.com/api';
 const API_KEY = '28612573-96fa08825695f2b47097a163d';
 const PARAM = 'per_page=40&orientation=horizontal&image_type=photo&safesearch=true';
 
-class FetchImagesService {
+export class FetchImagesService {
     constructor(){
         this.searchQuery = '';
         this.page = 1;
