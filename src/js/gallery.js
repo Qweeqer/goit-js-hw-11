8f16c65d54ac8d7fb38626601eb6acb34542a965
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-07-15 14:59:18 +0300

    comment file

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,70 +1,70 @@
 'use strict';
-import './css/styles.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-import { createGalleryCards } from './createGalleryCards';
+// import './css/styles.css';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
+// import { createGalleryCards } from './createGalleryCards';
 
 
-import { pixabayAPI } from './pixabayAPI';
-import { LoadMoreBtn } from './loadMoreBtn';
-// import { makeImageMarkup } from './js/markupService';
+// import { pixabayAPI } from './pixabayAPI';
+// import { LoadMoreBtn } from './loadMoreBtn';
+// import { makeImageMarkup } from './markupService';
 
-import { getRefs } from './getRefs';
-const refs = getRefs();
+// import { getRefs } from './getRefs';
+// const refs = getRefs();
 
 
-const pixabayAPI = new PixabayAPI();
+// const pixabayAPI = new PixabayAPI();
 
-const handleSubmit = event => {
-  event.preventDefault();
+// const handleSubmit = event => {
+//   event.preventDefault();
 
-  const { query } = event.currentTarget.elements;
-  searchValue = query.value.trim();
+//   const { query } = event.currentTarget.elements;
+//   searchValue = query.value.trim();
 
-  if (!searchValue) {
-    Notify.failure('enter text!');
-    return;
-  }
+//   if (!searchValue) {
+//     Notify.failure('enter text!');
+//     return;
+//   }
 
-  pixabayAPI.page = 1;
-  refs.list.innerHTML = '';
-  refs.loadMoreBtn.classList.add('is-hidden');
+//   pixabayAPI.page = 1;
+//   refs.list.innerHTML = '';
+//   refs.loadMoreBtn.classList.add('is-hidden');
 
-  pixabayAPI = searchValue;
+//   pixabayAPI = searchValue;
 
-  pixabayAPI
-    .getImages()
-    .then(({ total, total_pages: totalPages, results: images }) => {
-      if (images.length === 0) {
-        Notify.failure(`Images by ${searchValue} not found!`);
-        query.value = '';
-        return;
-      }
+//   pixabayAPI
+//     .getImages()
+//     .then(({ total, total_pages: totalPages, results: images }) => {
+//       if (images.length === 0) {
+//         Notify.failure(`Images by ${searchValue} not found!`);
+//         query.value = '';
+//         return;
+//       }
 
-      const markup = createGalleryCards(images);
-      refs.list.insertAdjacentHTML('beforeend', markup);
+//       const markup = createGalleryCards(images);
+//       refs.list.insertAdjacentHTML('beforeend', markup);
 
-      if (pixabayAPI.page === 1 && totalPages !== 1) {
-        refs.loadMoreBtn.classList.remove('is-hidden');
-      }
-    });
-};
+//       if (pixabayAPI.page === 1 && totalPages !== 1) {
+//         refs.loadMoreBtn.classList.remove('is-hidden');
+//       }
+//     });
+// };
 
-const handleClickLoadMore = () => {
-  pixabayAPI.updadePage();
+// const handleClickLoadMore = () => {
+//   pixabayAPI.updadePage();
 
-  pixabayAPI
-    .getImages()
-    .then(({ total, total_pages: totalPages, results: images }) => {
-      if (pixabayAPI.page >= totalPages) {
-        refs.loadMoreBtn.classList.add('is-hidden');
-        Notify.info('The end this collection!');
-      }
+//   pixabayAPI
+//     .getImages()
+//     .then(({ total, total_pages: totalPages, results: images }) => {
+//       if (pixabayAPI.page >= totalPages) {
+//         refs.loadMoreBtn.classList.add('is-hidden');
+//         Notify.info('The end this collection!');
+//       }
 
-      const markup = createGalleryCards(images);
-      refs.list.insertAdjacentHTML('beforeend', markup);
-    });
-};
-refs.loadMoreBtn.addEventListener('click', handleClickLoadMore);
-refs.form.addEventListener('submit', handleSubmit);
+//       const markup = createGalleryCards(images);
+//       refs.list.insertAdjacentHTML('beforeend', markup);
+//     });
+// };
+// refs.loadMoreBtn.addEventListener('click', handleClickLoadMore);
+// refs.form.addEventListener('submit', handleSubmit);
