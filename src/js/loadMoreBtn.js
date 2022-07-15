49ed3c3cacaff8f127babe1e78accd6efe5a3598
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-07-15 14:58:13 +0300

    add rootselector

--- src/js/loadMoreBtn.js
+++ src/js/loadMoreBtn.js
@@ -1,31 +1,25 @@
 export class LoadMoreBtn {
-    constructor({ selektor, hidden = false }) {
-        this.refs = this.getRefs(selektor);
-
+    constructor({ rootSelektor, hidden = false }) {
+        this.refs = this.getRefs(rootSelektor);
         hidden && this.hide();
     }
-
-    getRefs(selektor) {
+    getRefs(rootSelektor) {
         const refs = {};
-        refs.button = document.querySelector(selektor);
+        refs.button = document.querySelector(rootSelektor);
         refs.label = refs.button.querySelector('.label');
         return refs;
     }
-
     disabled() {
         this.refs.button.disabled = true;
         this.refs.label.textContent = 'Loading...';
     }
-
     enable() {
         this.refs.button.disabled = false;
         this.refs.label.textContent = 'Load more';
     }
-
     show() {
         this.refs.button.classList.remove('is-hidden');
     }
-
     hide() {
         this.refs.button.classList.add('is-hidden');
     }
